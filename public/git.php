0814bd7b2d2210ea8b75ac3adfa1176ed2b0176f
dolphin <416509859@qq.com>
2017-08-30 16:57:28 +0800

    some

--- public/git.php
+++ public/git.php
@@ -2,13 +2,7 @@
 
     $output = array();
 
-<<<<<<< HEAD
-    exec('git pull origin master 2>&1', $output);
-
-    // exec('pwd', $output);
-=======
-    exec('sudo git pull origin master', $output);
->>>>>>> ba4530232ba612d89ec89582fda8acaf4a258863
+    exec('sudo git pull origin master 2>&1', $output);
 
     var_dump($output);
 
